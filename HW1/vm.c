8400793eb654f68bcf696b276276bd911cbc2f90
eddymarg <103199715+eddymarg@users.noreply.github.com>
2023-09-26 20:18:54 -0400

    Create vm.c

---
+++ HW1/vm.c
@@ -0,0 +1,12 @@
+// Edrienne Gregana, (everyone else add names pls)
+// SRM implementation
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static int PC; // program counter
+static int SP; // stack pointer
+
+int main(){
+    
+}
