92948c6471c07ee048ff95a22d1c3a23aab7eab6
AndrewZ496 <144551615+AndrewZ496@users.noreply.github.com>
2023-09-26 21:10:21 -0400

    Update vm.c

--- HW1/vm.c
+++ HW1/vm.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include "machine_types.h"
 #include "instruction.h"
-/ / a s i z e f o r t h e memory ( 2 ^ 1 6 b y t e s = 64K )
+// a s i z e f o r t h e memory ( 2 ^ 1 6 b y t e s = 64K )
 #define MEMORY_SIZE_IN_BYTES (65536 - BYTES_PER_WORD)
 #define MEMORY_SIZE_IN_WORDS (MEMORY_SIZE_IN_BYTES / BYTES_PER_WORD)
 static union mem_u {
